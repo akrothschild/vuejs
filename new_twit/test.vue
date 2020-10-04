5e185a57aaa1e9293852827e89d62ad751aaa6e1
Aydar <51855086+st-aydar@users.noreply.github.com>
2020-10-04 21:47:34 +0200

    new_twit

---
+++ new_twit/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    {{ user.username }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    followers: 0,
+    User: {
+      id: 1,
+      username: '_RichardWindsor',
+      firstName: 'Richard',
+      lastName: 'Windsor',
+      email: 'rw@pm.me',
+      isAdmin: true,
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
